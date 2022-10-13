950367ee9bfc6afcb8170d885cde3fda032651a6
XavierTwitty <x.twitty@gmail.com>
2022-10-13 07:47:16 -0700

    server catch all

--- api/server.js
+++ api/server.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
 module.exports = server;
